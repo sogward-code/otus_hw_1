885d1d52e9abf6693e0b6326f7eb6437a73d8a90
Danil Mashchenko <danil.mashchenko@playrix.com>
2025-06-03 18:50:50 +0200

    fix file with flake8

--- fix_me.py
+++ fix_me.py
@@ -1,9 +1,8 @@
 def calculate_average(nums):
     total = sum(nums)
-    count = len(nums)
-    return total / count
-
+    count = len(numbers)
+    average = total / count
 
 nums = [10, 15, 20]
-result = calculate_average(nums)
-print("The average is:", result)
+result = calculat_average(nums)
+Print("The average is:", result)
